ec4abfc3398f514df1749225039a38a6c1795cc8
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-03-06 10:36:06 -0500

    Finished histogram exercises complete

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ console.log(`There are ${characters.length} characters in the array.`);
 // How many characters names start with "A"?
 let namesA = characters.filter(val => {
     return val.name[0] === "A";
-})
+});
 console.log("Character names that start with letter A");
 console.log(namesA);
 
@@ -11,7 +11,7 @@ console.log(namesA);
 // How many characters names start with "Z"?
 let namesZ = characters.filter(val => {
     return val.name[0] === "Z";
-})
+});
 console.log("Character names that start with letter Z");
 console.log(namesZ);
 
@@ -19,7 +19,7 @@ console.log(namesZ);
 // How many characters are dead?
 let deadCharacters = characters.filter(val => {
     return val.died != "";
-})
+});
 console.log("Characters that are dead");
 console.log(deadCharacters);
 
@@ -36,7 +36,7 @@ console.log(mostTitles[1]);
 // How many are Valyrian?
 let cultureValyrian = characters.filter(val => {
     return val.culture === "Valyrian";
-})
+});
 console.log("Valyrian peoples");
 console.log(cultureValyrian);
 
@@ -44,7 +44,9 @@ console.log(cultureValyrian);
 // What actor plays "Hot Pie" (and don't use IMDB)?
 let actorHotPie = characters.filter(val => {
     return val.name === "Hot Pie";
-})
+});
+hotPie = actorHotPie[0];
+console.log(hotPie.playedBy[0]);
 console.log("Actor who plays Hot Pie");
 console.log(actorHotPie);
 
@@ -53,7 +55,7 @@ console.log(actorHotPie);
 // How many characters are *not* in the tv show?
 let notInShow = characters.filter(val => {
     return val.tvSeries[0] === "";
-})
+});
 console.log("Characters not in the Show");
 console.log(notInShow);
 
@@ -63,15 +65,26 @@ console.log(notInShow);
 let lastNameTargaryen = characters.filter(val => {
     let check = val.name.split(' ');
     return check[1] === "Targaryen";
-})
+});
 console.log("Characters with the last name Targaryen");
 console.log(lastNameTargaryen);
 
 
 
-// Create a histogram of the houses (it's the "allegiances" key)#
-let histogram = characters.filter(val => {
-    return ;
-})
-console.log("Histogram of the houses");
-console.log(histogram);
+// Create a histogram of the houses (it's the "allegiances" key)
+function createHouseHistogram(characters) {
+    const histogram = {};
+
+    console.log(characters[0].allegiances);
+    characters.forEach(function (person) {
+        person.allegiances.forEach(function (house) {
+            if (histogram[house]) {
+                histogram[house] += 1
+            } else {
+                histogram[house] = 1;
+            }            
+        });
+    });
+    return histogram;
+}
+createHouseHistogram(characters)
